b72815b1c3ddd7929efce074e15e86edbe28b45a
Brianna <bpopson@gmail.com>
2016-03-01 08:48:07 -0800

    create second passing test for getEnroll_date

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -36,6 +36,21 @@
             $this->assertEquals("Bob Morley", $result);
         }
 
+        function test_getEnroll_date()
+        {
+            //Arrange
+            $name = "Bob Morley";
+            $enroll_date = "2014-8-20";
+            $id = null;
+            $test_student = new Student ($name, $enroll_date, $id);
+
+            //Act
+            $result = $test_student->getEnroll_date();
+
+            //Assert
+            $this->assertEquals("2014-8-20", $result);
+        }
+
     }
 
 ?>
