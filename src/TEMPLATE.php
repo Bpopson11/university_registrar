b38b1e23ce1f2c4c79af3cd254ea9d6c6a5cde21
Brianna <bpopson@gmail.com>
2016-03-01 08:41:43 -0800

    create inital Student.php functions

--- src/TEMPLATE.php
+++ src/TEMPLATE.php
@@ -38,5 +38,56 @@ class Student
         return $this->id;
     }
 
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO students (name, enroll_date) VALUES ('{$this->getName()}', '{$this->getEnroll_date()}')");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM students;");
+    }
+
+    static function getAll()
+    {
+        $returned_students = $GLOBALS['DB']->query("SELECT * FROM students;");
+        $students = array();
+        foreach ($returned_students as $student) {
+            $name = $student['name'];
+            $enroll_date = $studnet['enroll_date'];
+            $id = $student['id'];
+            $new_student = new Student($name, $enroll_date, $id);
+            array_push($students, $new_student);
+        }
+        return $students;
+    }
+
+    function deleteStudent()
+    {
+        $GOBALS['DB']->exec("DELETE FROM students WHERE id = {$this->getId()};");
+    }
+
+    function updateStudentName($new_name)
+    {
+        $GLOBALS['DB']->exect("UPDATE students SET name = '{$new_name}' WHERE id = {$this->getId()};");
+        $this->setName($new_name);
+    }
+
+    function findStudent($search_id)
+    {
+        $found_student = null;
+        $students = Student::getAll();
+        foreach($students as $student) {
+            $student_id = $student->getId();
+            if ($student_id == $search_id) {
+                $found_student = $student;
+            }
+        }
+        return $found_student;
+    }
+
+    
+
   }
 ?>
