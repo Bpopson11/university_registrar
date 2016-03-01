636bbff2ee89687a8c95dfd9e99b622a73d3e158
Brianna <bpopson@gmail.com>
2016-03-01 09:14:31 -0800

    set up initial profile add functionality on index.html.twig

--- app/app.php
+++ app/app.php
@@ -1,19 +1,36 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Inventory.php";
+    require_once __DIR__."/../src/Student.php";
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=my_inventory';
+    $server = 'mysql:host=localhost;dbname=university';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
+
+
+  	use Symfony\Component\HttpFoundation\Request;
+  	Request::enableHttpMethodParameterOverride();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
 
-    
+    //home page to show students
+
+    $app->get("/", function() use ($app)
+    {
+        return $app['twig']->render('index.html.twig', array('students' => Student::getAll()));
+    });
+
+    $app->post("/students", function() use ($app)
+    {
+        $student = new Student($_POST['name'], $_POST['enroll_date']);
+        $student->save();
+        return $app['twig']->render('index.html.twig', array('students' => Student::getAll()));
+    });
+
 
       return $app;
 
