b86541fd3d9357d362680a3157f804d3ebdff5c9
Brianna <bpopson@gmail.com>
2016-03-01 14:15:42 -0800

    create an add course option on students.html.twig

--- app/app.php
+++ app/app.php
@@ -18,7 +18,8 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
-    //home page to show students
+    $app['debug'] = true;
+
 
     $app->get("/", function() use ($app)
     {
@@ -49,7 +50,33 @@
         return $app['twig']->render('index.html.twig', array('courses' => Course::getAll(), 'students' => Student::getAll()));
     });
 
+    $app->get("/students/{id}", function($id) use ($app)
+    {
+        $student = Student::findStudent($id);
+        return $app['twig']->render('students.html.twig', array('student' => $student, 'students' => Student::getAll(), 'courses'=>$student->getCourses(), 'all_courses'=> Course::getAll()));
+    });
 
+    $app->get("/courses/{id}", function() use ($app)
+    {
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->post("/delete_all_students", function() use ($app) {
+        Student::deleteAll();
+        return $app['twig']->render('index.html.twig', array('courses' =>Course::getAll(), 'students' =>Student::getAll()));
+    });
+
+    $app->post("/delete_all_courses", function() use ($app) {
+        Course::deleteAll();
+        return $app['twig']->render('index.html.twig', array('students' =>Student::getAll(), 'courses' =>Course::getAll()));
+    });
+
+    $app->post("/add_course", function() use ($app) {
+        $course = Course::findCourse($_POST['course_id']);
+        $student = Student::findStudent($_POST['student_id']);
+        $student->addCourse($course);
+        return $app['twig']->render('students.html.twig', array('student' => $student, 'students' => Student::getAll(), 'courses'=>$student->getCourses(), 'all_courses'=> Course::getAll()));
+    });
 
 
       return $app;
